65444631a1606533c7226362310891951a4fba51
Gabi Lago Blasco <gabilagoblasco.es@gmail.com>
2019-04-25 21:01:01 +0200

    Initial Commit

---
+++ Ledger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ledger
+//
+//  Created by Gabriel Lago Blasco on 25/04/2019.
+//  Copyright © 2019 Gabi Lago Blasco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
